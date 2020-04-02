7105c594aa03b0ee7e9f05aa845890249b950f37
Bwoodruff4 <blakewoodruff4@gmail.com>
2020-04-02 12:13:58 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Company
     attr_accessor :name, :size
 
@@ -9,8 +11,45 @@ end
 
 companies = [
     Company.new('Alpha', 30),
-    Company.new('Beta, 300),
-    Company.new('Delta, 3000)
+    Company.new('Beta', 300),
+    Company.new('Delta', 3000)
 ]
 
 ### Your code below this
+list_of_companies_with_size=[]
+
+companies.each do|company|
+    list_of_companies_with_size << "#{company.name}-#{company.size}"
+end
+
+p list_of_companies_with_size
+
+company_sum=companies.reduce(0) do|acc,company|
+    acc+company.size
+end
+
+p company_sum
+
+companies_over_100 = companies.select do|company|
+    company.size > 100
+end
+
+p companies_over_100
+
+beta_company = companies.find do|company|
+    company.name == "Beta"
+end
+
+p beta_company
+
+largest_company = companies.max_by do|company|
+    company.size
+end
+
+p largest_company
+
+companies_largest_to_smallest = companies.sort do|company|
+    company.size
+end
+
+p companies_largest_to_smallest
